5d4f99b22560b6985aaec7ad45ec5270e6ded7c3
David Fox Powell <dtothefp@gmail.com>
2014-05-16 00:00:48 -0400

    move template compilation into controller

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
--- public/src/angular/modules/gridster/appendTemplateDirective.js
+++ public/src/angular/modules/gridster/appendTemplateDirective.js
@@ -5,11 +5,11 @@ app.directive('appendTemplate', function ($rootScope, $compile) {
       template: "="
     },
     link: function postlink(scope, elm, attrs) {
-      var compiled = $compile(scope.template)($rootScope);
-      // attempt to add dynamically add the gridster-item directive
-      //compiled.attr("gridster-item", "template");
-      compiled.attr("id", Math.random());
-      elm.append(compiled);
+      // var compiled = $compile(scope.template)($rootScope);
+      // // attempt to add dynamically add the gridster-item directive
+      // //compiled.attr("gridster-item", "template");
+      // compiled.attr("id", Math.random());
+      elm.append(scope.template);
       var innerElm = elm.children()[elm.children().length - 1];
     }
   };
